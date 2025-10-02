bb4211cfbf797d375cf07ccc145c185d4c26b588
daniel4191 <daniell8282@naver.com>
2025-10-02 14:43:19 +0900

    기본구조 연결 모두

--- django2/urls.py
+++ django2/urls.py
@@ -20,12 +20,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import index
-from blog.views import post_list
+from blog.views import post_list, post_detail, post_add
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
-    path("posts/", post_list)
+    path("posts/", post_list),
+    path("posts/<int:post_id>/", post_detail),
+    path("posts/add/", post_add)
 ]
 urlpatterns += static(
     prefix=settings.MEDIA_URL,
